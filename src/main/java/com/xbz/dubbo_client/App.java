e58d7c1c7cac0d250164d0dbba1aeca4d094b808
liupenggoodman6 <1948966136@qq.com>
2020-08-05 15:24:49 +0800

    xxx

---
+++ src/main/java/com/xbz/dubbo_client/App.java
@@ -0,0 +1,13 @@
+package com.xbz.dubbo_client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
